98451aa0ff1693fd113476d02988745ccc95f8e3
Victor Hornet <victorhornet@tudelft.nl>
2025-05-25 12:23:59 +0200

    Download data from url and update requirements

--- model_training/dataset.py
+++ model_training/dataset.py
@@ -4,17 +4,38 @@ from typing import Optional
 from lib_ml.preprocessing import TextPreprocessor
 from loguru import logger
 import pandas as pd
+import requests
 from sklearn.feature_extraction.text import CountVectorizer
 import typer
 
-from model_training.config import PROCESSED_DATA_DIR, RAW_DATA_DIR
+from model_training.config import DEFAULT_TRAINING_DATA_URL, PROCESSED_DATA_DIR, RAW_DATA_DIR
 
 app = typer.Typer()
 
 
-def preprocess_data(input_path: Path, output_path: Optional[Path] = None):
+def preprocess_data(
+    input_dataset_url: str = DEFAULT_TRAINING_DATA_URL,
+    output_path: Optional[Path] = None,
+):
+    file_name = Path(input_dataset_url.split("/")[-1])
+    local_input_path = RAW_DATA_DIR / file_name
+
+    RAW_DATA_DIR.mkdir(parents=True, exist_ok=True)
+
+    logger.info(f"Downloading dataset from {input_dataset_url} to {local_input_path}...")
+    try:
+        response = requests.get(input_dataset_url, stream=True)
+        response.raise_for_status()
+        with open(local_input_path, "wb") as f:
+            for chunk in response.iter_content(chunk_size=8192):
+                f.write(chunk)
+        logger.success(f"Successfully downloaded dataset to {local_input_path}")
+    except requests.exceptions.RequestException as e:
+        logger.error(f"Error downloading dataset from {input_dataset_url}: {e}")
+        raise typer.Exit(code=1)
+
     logger.info("Loading raw data...")
-    dataset = pd.read_csv(input_path, delimiter="\t", quoting=3)
+    dataset = pd.read_csv(local_input_path, delimiter="\t", quoting=3)
 
     logger.info("Processing dataset...")
     preprocessor = TextPreprocessor()
@@ -39,10 +60,10 @@ def preprocess_data(input_path: Path, output_path: Optional[Path] = None):
 
 @app.command()
 def main(
-    input_path: Path = RAW_DATA_DIR / "a1_RestaurantReviews_HistoricDump.tsv",
+    input_dataset_url: str = DEFAULT_TRAINING_DATA_URL,
     output_path: Path = PROCESSED_DATA_DIR / "dataset.csv",
 ):
-    preprocess_data(input_path, output_path)
+    preprocess_data(input_dataset_url, output_path)
 
 
 if __name__ == "__main__":
